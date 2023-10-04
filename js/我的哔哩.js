05360264ca04637a611e2c762963c400cd535b92
weigs <358832778@qq.com>
2023-10-04 11:34:35 +0000

    更新 'js/我的哔哩.js'

--- js/我的哔哩.js
+++ js/我的哔哩.js
@@ -125,6 +125,8 @@ var rule = {
             'Referer': 'https://live.bilibili.com',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
         };
+
+        result.danmaku = 'https://api.bilibili.com/x/v1/dm/list.so?oid=' + vod.cid;
         result.contentType = 'video/x-flv';
         input = result
     `,
